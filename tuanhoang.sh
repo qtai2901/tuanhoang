c5e4dd4dd581bf84723481ddbf4d699910b3c14c
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2022-04-01 20:22:49 +0700

    Update tuanhoang.sh

--- tuanhoang.sh
+++ tuanhoang.sh
@@ -135,11 +135,11 @@ read -p "Giới hạn thiết bị :" DeviceLimit
   echo "-------------------------------"
   
   #CertDomain:
-read -p "ip hoăc domain:" CertDomain
-  [ -z "${CertDomain}" ] && CertDomain="0"
-  echo "-------------------------------"
-  echo "CertDomain: ${CertDomain}"
-  echo "-------------------------------"
+#read -p "ip hoăc domain:" CertDomain
+  #[ -z "${CertDomain}" ] && CertDomain="0"
+ # echo "-------------------------------"
+ # echo "CertDomain: ${CertDomain}"
+ # echo "-------------------------------"
   
   
 }
@@ -234,7 +234,7 @@ EOF
   sed -i "s|ApiKey:.*|ApiKey: \"${ApiKey}\"|" ./config.yml
   sed -i "s|NodeID:.*|NodeID: ${node_id}|" ./config.yml
   sed -i "s|DeviceLimit:.*|DeviceLimit: ${DeviceLimit}|" ./config.yml
-  sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
+  #sed -i "s|CertDomain:.*|CertDomain: \"${CertDomain}\"|" ./config.yml
   }
 
 # Install docker and docker compose
